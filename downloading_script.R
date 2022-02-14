e879549f9bc88b1155be897f11147d3bb64fac17
Man Matěj <matej.man@ibot.cas.cz>
2022-02-14 13:19:33 +0100

    Added RBCN stations

--- downloading_script.R
+++ downloading_script.R
@@ -4,7 +4,8 @@ library(stringr)
 library(xml2)
 library(purrr)
 
-## URL mining --------------------------------------------
+## 1. Daily data 123/1998 sb  --------------------------------------------
+## URL mining
 
 # scrap element codes  
 base <- "https://www.chmi.cz/files/portal/docs/meteo/ok/open_data/RDATA/denni_data_cs.html"
@@ -65,7 +66,7 @@ nam<-paste(n,a,m,sep="_")
 nam<-make.names(nam)
 sum(duplicated(nam))
 
-## Downloading --------------------------------------------
+## Downloading
 
 setwd("d:/temp_meteo/") # where should be zips stored
 
@@ -73,3 +74,36 @@ safe_download <- purrr::safely(~ download.file(.x , .y, mode = "wb",quiet = F)) 
 purrr::walk2(.x=result.zip,nam, .f=safe_download) # walk through valid urls and save.
 
 
+## 2. Daily data RBCN -------------------------------------
+# scrap station pages  
+base <- "https://www.chmi.cz/files/portal/docs/meteo/ok/denni_data/Denni_data_ze_stanic/ddata_cs.html"
+page <- read_html(base)
+
+page.list<-page %>%
+  html_elements("a") %>%  # find all links on the page
+  html_attr("href")       # get the urls
+
+a<-strsplit(page.list,"/") # parse result for extracting pages code
+pcode<-sapply(a,function(x) x[2]) # extract pages code
+pcode<-unique(pcode)[-1] # remove first code (unused) 
+
+## generate urls of station pages 
+pages<-sapply(pcode,function(x) paste0("https://www.chmi.cz/files/portal/docs/meteo/ok/denni_data/Denni_data_ze_stanic/",x))
+
+## get urls for XLS data download
+xls.url<-list() #prepare empty list to store urls
+nam<-list() #prepare empty list to store file names
+for (i in 1:length(pages)){
+  p <- read_html(pages[i])
+  page.list1<-p %>%
+    html_elements("a") %>%  # find all links on the page
+    html_attr("href")       # get the urls
+  a<-strsplit(page.list1,"/") # parse result for extracting xls table names
+  table.file<-sapply(a,function(x) x[3]) # extract table name
+  xls.url[i]<-paste0("https://www.chmi.cz/files/portal/docs/meteo/ok/denni_data/Denni_data_ze_stanic/files/",table.file)
+  nam[i]<-table.file
+}
+
+setwd("d:/temp_meteo/") # where should be zips stored
+safe_download <- purrr::safely(~ download.file(.x , .y, mode = "wb",quiet = F)) # construct envelope to skip not existing urls
+purrr::walk2(.x=xls.url,nam, .f=safe_download) # walk through valid urls and save.
